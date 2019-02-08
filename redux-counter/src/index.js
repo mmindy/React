8aa60a623261f9e2cd41735d71e4b94453e9d012
minji <flowing_mj@naver.com>
2019-02-08 18:49:06 +0900

    practice redux

---
+++ redux-counter/src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './containers/App';
+import * as serviceWorker from './serviceWorker';
+
+import {createStore} from 'redux';
+import reducers from './reducers';
+import Provider from 'react-redux';
+
+const store = createStore(reducers);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+  , document.getElementById('root'));
+
+serviceWorker.unregister();
